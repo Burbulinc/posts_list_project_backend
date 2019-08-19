e92c1e2f116d6a4c328ab885faec7b566fb4a6d2
Dmitrii Serdun <serdun.dmitrii@steelkiwi.com>
2019-08-19 21:34:12 +0300

    -added functionality for post message

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, redirect, flash, url_for
+from flask import render_template, redirect, flash, url_for, request
 
-from app import app
+from app import app, db
 from app.forms import SendPostForm
 from app.models import Post
 
@@ -9,12 +9,16 @@ from app.models import Post
 @app.route('/index')
 def index():
     posts = Post.query.all()
-    postsForm=SendPostForm()
-    return render_template('index.html',posts=posts,form=postsForm)
+    postsForm = SendPostForm()
+    return render_template('index.html', posts=posts, form=postsForm)
 
-@app.route('/send_post',methods=['POST'])
+
+@app.route('/send_post', methods=['POST'])
 def sendPost():
-    return "Hello"
+    usrname = request.form['username']
+    text = request.form['text']
 
-
-
+    u = Post(user_name=usrname, text=text)
+    db.session.add(u)
+    db.session.commit()
+    return redirect('/index')
